eaefaff6ccd6444e8e25530dcaef637a5c3675da
matthewkuchurean <matthew.kuchurean@flemingcollege.ca>
2023-02-15 12:53:32 -0500

    Update lab4.py

--- lab4.py
+++ lab4.py
@@ -1,58 +1,54 @@
-import log_analysis import get_log_file_path_from_cmd_line, filter_log_by_regex
-import pandas as pd
 def main():
-    log_file = get_log_file_path_from_cmd_line()
-    records = filter_log_by_regex(log_file, 'sshd', print_records=True)
-# records = filter_log_by_regex(log_file, 'invalid user.*220.195.35.40', print_records=True)
-# records = filter_log_by_regex(log_file, 'error', print_records=True)
-# records = filter_log_by_regex(log_file, 'pam', print_records=True) 
-# records, data = filter_log_by_regex(log_file, 'SRC=(.*?) DST=(.*?) LEN=(.*?)', print_records=True)
-port_traffic = tally_port_traffic(log_file)
+    log_file = get_log_file_path_from_cmd_line(1)
+    port_traffic = tally_port_traffic(log_file)
+    generate_port_traffic_report(log_file, 40686)
+    generate_invalid_user_report(log_file)
+    generate_source_ip_log(log_file, ip_address='220.195.35.40')
 
-for port_num, count in port_traffic.items():
-    
-    if count >= 100:
-        generate_port_traffic_report(log_file, port_num)
+    for port_num, count in port_traffic.items():
+        if count >=100:
+            generate_port_traffic_report(log_file, port_num)
 
-pass 
-   
-# TODO: Step 8
+    pass
+
 def tally_port_traffic(log_file):
-    filter_log_by_regex(log_file, r'DPT=(.+?) ')[1] 
+    data = filter_log_by_regex(log_file, r'DPT=(.+?) ')[1]
     port_traffic = {}
     for d in data:
-        port = d[0] 
-        port_traffic[port] = port_traffic.get(port, 0) + 1     
+        port = d[0]
+        port_traffic[port] = port_traffic.get(port, 0) + 1
     return port_traffic
 
-# TODO: Step 9
 def generate_port_traffic_report(log_file, port_number):
     
-    regex = r'(.{6}) (.{8}) .*SRC=(.+) DST=(.*+?) .+SPT=(.+) ' + f'DPT=({port_number}) '
-    records, data = filter_log_by_regex(log_file,regex)[1] 
-    
+    regex = r'(.{6}) (.{8}) .*SRC=(.+) DST+(.+?) .+SPT=(.+) ' + f'DPT=({port_number}) '
+    data = filter_log_by_regex(log_file, regex)[1]
+
     report_df = pd.DataFrame(data)
-    header_row = ('Data', 'Time', 'Source IP address', 'Destnation IP address', 'Source Port', 'Destination Port' )
+    header_row = ('Date', 'Time', 'Source IP Address', 'Destination IP Address', 'Source Port', 'Destination Port')
     report_df.to_csv(f'destination_port_{port_number}_report.csv', index=False, header=header_row)
-    
-    
+
     return
 
-# TODO: Step 11
 def generate_invalid_user_report(log_file):
-    report = r'(.{6}) (.{8}) .*SRC=(.+) DST=(.*+?) .+SPT=(.+) ' + f'DPT=({port_number}) '
-    records, data = filter_log_by_regex(log_file,report)[1] 
-    
-    report_df = pd.DataFrame(data)
-    header_row = ('Data', 'Time', 'Source IP address', 'Destnation IP address', 'Source Port', 'Destination Port' )
-    report_df.to_csv(f'destination_port_{port_number}_report.csv', index=False, header=header_row)
-    
-    
+    regex = r'(.{6}) (.{8}) .*Invalid user (.+) from +(.+)'
+    gen_report = filter_log_by_regex(log_file, regex)[1]
+
+    report_df2 = pd.DataFrame(gen_report)
+    header_row = ('Date', 'Time', 'Username', 'IP Address')
+    report_df2.to_csv('invalid_users.csv', index=False, header=header_row)
+ 
     return
 
-# TODO: Step 12
 def generate_source_ip_log(log_file, ip_address):
-    return
 
+    regex = rf'(.*SRC={ip_address}.*)'
+    gen_iplog = filter_log_by_regex(log_file, regex)[1]
+
+    report_df3 = pd.DataFrame(gen_iplog)
+    ip_address = re.sub('\.', '_', ip_address)
+    report_df3.to_csv(f'source_ip_{ip_address}.log', index=False, header=False)
+
+    return
 if __name__ == '__main__':
     main()
